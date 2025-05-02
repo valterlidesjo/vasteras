138faab342dbc512df2556d72657799cfda036c1
Malte Rozenbeek <malte.rozenbeek@gmail.com>
2025-05-02 11:01:00 +0200

    💄

---
+++ src/pages/AboutUs.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+const AboutUs = () => {
+  return (
+    <>
+      <div>
+        <p>Om Hotell Västerås</p>
+        <p>
+          Upplev en personlig och bekväm vistelse mitt i Västerås pulserande
+          hjärta. Vårt familjeägda 3-stjärniga hotell erbjuder 34 unikt inredda
+          rum, perfekta för både affärsresenärer och semesterfirare. Njut av
+          närheten till stadens sevärdheter och upplev en varm, välkomnande
+          atmosfär. Med moderna bekvämligheter och en genuin känsla av
+          hemtrevnad är Hotel Västerås mer än bara ett hotell – det är din oas i
+          staden.
+        </p>
+      </div>
+      <div>
+        <p>Fler än bara familjen? Vi har plats för er!</p>
+        <p>
+          Planerar du en gruppresa, en föreningsträff eller en konferens? På
+          Hotel Västerås välkomnar vi alla typer av sällskap! Vårt rymliga
+          samlingsrum är perfekt för möten, workshops eller sociala
+          tillställningar. Och oavsett om ni är här för arbete eller nöje, kan
+          ni alltid räkna med bekvämligheter som fri tillgång till Wi-Fi, ett
+          praktiskt gästkök och en tvättstuga.
+        </p>
+        <p>
+          Dessutom erbjuder vi parkering på baksidan, i mån av plats. Gör er
+          vistelse i Västerås enkel och bekväm – tillsammans.
+        </p>
+        <p>För bokningar med större sällskap kontakta oss genom nedan! </p>
+      </div>
+    </>
+  );
+};
+
+export default AboutUs;
--- src/utils/Router.tsx
+++ src/utils/Router.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import NotFound from "../pages/NotFound";
 import MainLayout from "../layout/MainLayout";
+import AboutUs from "../pages/AboutUs";
 
 export const router = createBrowserRouter([
   {
@@ -12,6 +13,10 @@ export const router = createBrowserRouter([
       {
         path: "/",
         element: <HomePage />,
+      },
+      {
+        path: "/om-oss",
+        element: <AboutUs />,
       },
     ],
   },
