97eca0e397f105f918adbfe872e3d61b89538643
Malte Rozenbeek <malte.rozenbeek@gmail.com>
2025-05-02 10:41:39 +0200

    footer layout and styling added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./utils/Router";
-import { RouterProvider } from "react-router-dom";
-import { router } from "./utils/Router";
 import "./styles/global/global.scss";
 
 function App() {
