b1427e57719738c660b33f22b5dbc840238e8f10
valterlidesjo <valle.lidesjo@gmail.com>
2025-05-05 09:00:59 +0200

    desk done aswell

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -5,11 +5,22 @@ interface ButtonProps {
   text: string;
   onClick: () => void;
   width: string;
+  maxWidth?: string;
   icon?: React.ReactNode;
 }
-const Button: React.FC<ButtonProps> = ({ text, onClick, width, icon }) => {
+const Button: React.FC<ButtonProps> = ({
+  text,
+  onClick,
+  width,
+  icon,
+  maxWidth,
+}) => {
   return (
-    <button onClick={onClick} className="button" style={{ width: width }}>
+    <button
+      onClick={onClick}
+      className="button"
+      style={{ width: width, maxWidth: maxWidth }}
+    >
       {text}
       {icon && <span className="btn-icon">{icon}</span>}
     </button>
